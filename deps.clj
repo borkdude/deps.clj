d75e6fa7aa6002413c30d6d69362e5a3eafe4f39
Michiel Borkent <michielborkent@gmail.com>
2020-01-20 22:59:53 +0100

    Version bump

--- deps.clj
+++ deps.clj
@@ -11,7 +11,7 @@
 (set! *warn-on-reflection* true)
 
 (def version "1.10.1.502")
-(def deps-clj-version "0.0.7")
+(def deps-clj-version "0.0.8-SNAPSHOT")
 
 (defn shell-command
   "Executes shell command.
