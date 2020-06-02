be3c6fde899597e19e69161a7ebdd795860f3a75
Michiel Borkent <michielborkent@gmail.com>
2020-06-02 23:01:57 +0200

    v0.0.9

--- deps.clj
+++ deps.clj
@@ -13,7 +13,7 @@
 (set! *warn-on-reflection* true)
 
 (def version "1.10.1.507")
-(def deps-clj-version "0.0.9-SNAPSHOT")
+(def deps-clj-version "0.0.9")
 
 (defn shell-command
   "Executes shell command.
