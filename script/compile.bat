7cd68a45c42c32ba59d7eafaec103d7c752f8f26
Michiel Borkent <michielborkent@gmail.com>
2020-09-26 20:22:29 +0200

    Fix Windows artifact

--- script/compile.bat
+++ script/compile.bat
@@ -35,7 +35,7 @@ call %GRAALVM_HOME%\bin\native-image.cmd ^
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo Creating zip archive
-jar -cMf deps.clj-%DEPS_CLJ_VERSION%-windows-amd64.zip deps
+jar -cMf deps.clj-%DEPS_CLJ_VERSION%-windows-amd64.zip deps.exe
 
 echo Test run
 rm -rf ~/.deps.clj
