6636890388cdf779708806a93110b165577cd226
Michiel Borkent <michielborkent@gmail.com>
2022-03-02 10:54:50 +0100

    gen script

--- deps.bat
+++ deps.bat
@@ -20,7 +20,7 @@
 
 ;; see https://github.com/clojure/brew-install/blob/1.10.3/CHANGELOG.md
 (def version (delay (or (System/getenv "DEPS_CLJ_TOOLS_VERSION")
-                        "1.10.3.1082")))
+                        "1.10.3.1087")))
 
 (def deps-clj-version "0.0.22")
 
