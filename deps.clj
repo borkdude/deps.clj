a195e410bfd61b3befcab40915cf6c2e54b0b7ca
Michiel Borkent <michielborkent@gmail.com>
2022-11-15 18:09:05 +0100

    Version bump

--- deps.clj
+++ deps.clj
@@ -20,7 +20,7 @@
 (def version (delay (or (System/getenv "DEPS_CLJ_TOOLS_VERSION")
                         "1.11.1.1200")))
 
-(def deps-clj-version "1.11.1.1200")
+(def deps-clj-version "1.11.1.1201-SNAPSHOT")
 
 (defn warn [& strs]
   (binding [*out* *err*]
