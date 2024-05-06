084a1543b701a4eb51282bb90f62fb65ce150006
Michiel Borkent <michielborkent@gmail.com>
2024-05-06 10:54:47 +0200

    v1.11.3.1463

--- src/borkdude/deps.clj
+++ src/borkdude/deps.clj
@@ -16,7 +16,7 @@
 ;; see https://github.com/clojure/brew-install/blob/1.11.1/CHANGELOG.md
 (def ^:private version
   (delay (or (System/getenv "DEPS_CLJ_TOOLS_VERSION")
-             "1.11.2.1446")))
+             "1.11.3.1463")))
 
 (def ^:private cache-version "5")
 
